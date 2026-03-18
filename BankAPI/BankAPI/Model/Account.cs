0bd7d85e713bf591d52ecc37d90047cad3d8c6f2
Ashwin-28 <mailtoashwin28@gmsil.com>
2026-03-18 20:32:36 +0530

    Creared Models Customer and Account

---
+++ BankAPI/BankAPI/Model/Account.cs
@@ -0,0 +1,25 @@
+﻿namespace BankAPI.Model
+{
+    public class Account
+    {
+        public int AccountId { get; set; }
+
+        // Foreign key to the Customer
+        public int CustomerId { get; set; }
+
+        public string AccountNumber { get; set; }
+
+        // Savings or Current
+        public AccountType Type { get; set; }
+
+        public decimal Balance { get; set; }
+
+        // Active or Inactive
+        public Status Status { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        // Optional: Navigation property to link back to the Customer model in Entity Framework
+        // public Customer Customer { get; set; }
+    }
+}
---
+++ BankAPI/BankAPI/Model/Customer.cs
@@ -0,0 +1,20 @@
+﻿namespace BankAPI.Model
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public long PhoneNumber { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int Pincode { get; set; }
+
+        // Use the enums as property types
+        public GenderType Gender { get; set; }
+        public Status Status { get; set; }
+        public DateTime CreatedAt { get;set;}
+    }
+}
